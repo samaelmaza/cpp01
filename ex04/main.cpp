72d0569ebd205484b12d59fc137bc60796a8f42b
samael_maza <samael.reffers@gmail.com>
2026-02-13 12:18:48 +0100

    makefile corrected

---
+++ ex04/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+	// TODO: verifier les arguments
+	// TODO: ouvrir le fichier d'entree
+	// TODO: creer le fichier de sortie (<filename>.replace)
+	// TODO: lire le contenu, remplacer s1 par s2, ecrire dans le fichier de sortie
+	return 0;
+}
